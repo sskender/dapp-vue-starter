efe8f94ac8745b874bbacf13a6bf330cb89d00c1
sskender <sskender@users.noreply.github.com>
2020-12-29 01:43:29 +0100

    Added vuex store

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import "@babel/polyfill";
 import "mutationobserver-shim";
 import Vue from "vue";
 import "./plugins/bootstrap-vue";
+import store from "./store/index";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  store: store,
 }).$mount("#app");
